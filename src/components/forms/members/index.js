ef1a267b22b89f69a2b99e9a8b70f23f6ba5a4ab
Pjaijai <paul6a24@hotmail.com>
2024-05-29 22:13:53 -0400

    Create create and edit member form

---
+++ src/components/forms/members/index.js
@@ -0,0 +1,108 @@
+import React, { useState } from "react"
+
+import { createMember, editMember } from "../../../api"
+import TextInput from "../../inputs/text"
+import NumberInput from "../../inputs/number"
+
+const MemberForm = ({ isEdit, onSuccess, initialValues }) => {
+  if (isEdit && !initialValues) throw Error("should provide initial values")
+
+  const [name, setName] = useState(isEdit ? initialValues.name : "")
+  const [age, setAge] = useState(isEdit ? initialValues.age : "")
+  const [rating, setRating] = useState(isEdit ? initialValues.rating : "")
+  const [activities, setActivities] = useState(
+    isEdit ? initialValues.activities : ""
+  )
+  const [isSubmitting, setIsSubmitting] = useState(false)
+
+  const handleSubmit = async (event) => {
+    try {
+      event.preventDefault()
+      setIsSubmitting(true)
+
+      const splittedActivitiesArray = activities.split(/\s+/)
+
+      if (!name) return alert("Name is required")
+      if (!age) return alert("Age is required")
+      if (!rating) return alert("Rating is required")
+      if (!activities) return alert("Activities is required")
+
+      const data = {
+        name,
+        rating,
+        age,
+        activities: splittedActivitiesArray,
+      }
+
+      const create_res = await createMember(data)
+
+      if (isEdit) {
+        const edit_res = await editMember(data, initialValues.id)
+      } else {
+        const create_res = await createMember(data)
+      }
+
+      if (onSuccess) {
+        onSuccess()
+      }
+      setName("")
+      setAge("")
+      setRating("")
+      setActivities("")
+    } catch (err) {
+      alert(err)
+    } finally {
+      setIsSubmitting(false)
+    }
+  }
+
+  const handleNameChange = (e) => {
+    setName(e.target.value.trim())
+  }
+
+  const handleRatingChange = (e) => {
+    const value = e.target.value
+    if (value < 0 || value > 5) return
+    if (!isNaN(value)) {
+      setRating(value)
+    }
+  }
+
+  const handleAgeChange = (e) => {
+    const value = e.target.value
+    if (value < 0) return
+    if (!isNaN(value)) {
+      setAge(value)
+    }
+  }
+
+  const handleActivitiesChange = (e) => {
+    setActivities(e.target.value)
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <TextInput
+        placeholder={"Name"}
+        value={name}
+        onChange={handleNameChange}
+      />
+      <NumberInput placeholder={"Age"} value={age} onChange={handleAgeChange} />
+      <NumberInput
+        placeholder={"Rating"}
+        value={rating}
+        onChange={handleRatingChange}
+      />
+      <TextInput
+        placeholder={"Activities (use whitespace to separate)"}
+        value={activities}
+        onChange={handleActivitiesChange}
+      />
+      <button type="submit" disabled={isSubmitting}>
+        Submit
+      </button>
+    </form>
+  )
+}
+
+export default MemberForm
--- src/components/member-list/index.js
+++ src/components/member-list/index.js
@@ -1,14 +1,26 @@
-import React from "react"
+import React, { useState } from "react"
 
 import useSearchMembers from "../../hooks/api/useSearchMembers"
 
-import { ActivitiesContainer, Block, Filters } from "./style"
+import { ActivitiesContainer, Block, Container, Filters } from "./style"
 import SortMemberSelect from "../selects/sort-member"
 import TextInput from "../inputs/text"
 import NumberInput from "../inputs/number"
 import MemberTable from "../tables/member"
+import MemberForm from "../forms/members"
 
 const MemberList = () => {
+  const EDIT_MEMBER_INITIAL_VALUES = {
+    id: null,
+    name: null,
+    rating: null,
+    age: null,
+    activities: null,
+  }
+  const [editInitialValues, setEditInitialValues] = useState(
+    EDIT_MEMBER_INITIAL_VALUES
+  )
+
   const {
     data,
     handleNameChange,
@@ -23,12 +35,44 @@ const MemberList = () => {
     selectedActivities,
     handleSortingChange,
     sorting,
+    refetch,
   } = useSearchMembers()
+
+  const onCreateMemberSuccess = () => {
+    refetch()
+  }
+
+  const onEditMemberSuccess = () => {
+    setEditInitialValues({
+      EDIT_MEMBER_INITIAL_VALUES,
+    })
+
+    refetch()
+  }
+
+  const handleEditMemberClick = (values) => {
+    console.log(124, values)
+    const { id, age, activities, name, rating } = values
+    const activitiesString = activities.join(" ")
+
+    setEditInitialValues({
+      id,
+      age,
+      activities: activitiesString,
+      name,
+      rating,
+    })
+  }
 
   return (
     <Block>
       <h1>My Club's Members</h1>
-
+      <MemberForm onSuccess={onCreateMemberSuccess} isEdit={false} />
+      <MemberForm
+        onSuccess={onEditMemberSuccess}
+        isEdit={true}
+        initialValues={editInitialValues}
+      />
       <Filters>
         <TextInput
           placeholder={"Search for a member"}
@@ -68,7 +112,13 @@ const MemberList = () => {
         ))}
       </ActivitiesContainer>
 
-      <MemberTable data={data} loading={loading} />
+      <Container>
+        <MemberTable
+          data={data}
+          loading={loading}
+          onEditClick={handleEditMemberClick}
+        />
+      </Container>
     </Block>
   )
 }
